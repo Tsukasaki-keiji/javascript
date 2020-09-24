a1f436b79c62ada2ac7a90e5ad866dcab6509c41
塚﨑慶二 <tsukasakikeiji@MacBook-Pro.local>
2020-09-24 15:18:01 +0900

    while文

--- javascript 2章/app.js
+++ javascript 2章/app.js
@@ -1,10 +1,12 @@
-var orange = 100;
-var apple = 120;
+var max = 100;
+var num = 1;
+var count = 0;
 
-if (orange < apple) {
-  alert("みかんの値段がりんごより安い");
-} else if (orange == apple) {
-  alert("みかんとりんごが同じ値段");
-} else {
-  alert(みかんの値段がりんごより高い);
+while (num < max) {
+  num = num * 2;
+  count = count + 1;
 }
+
+alert(
+  "２を掛け続けて" + max + "を超えるのに必要立った回数は" + count + "回です"
+);
