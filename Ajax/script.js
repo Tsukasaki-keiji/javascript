b3c5373cb63d4e75fbbc3d9884bbe9c80153c42c
塚﨑慶二 <abcdquont1234@gmail.com>
2020-09-25 16:45:33 +0900

    通信成功追記

--- Ajax/script.js
+++ Ajax/script.js
@@ -1,8 +1,27 @@
-$('#tab-contents .tab[id != "tab1"]').hide();
+API_KEY = "41001644021fec3ee50ae0feae7f02f7";
 
-$("#tab-menu a").on("click", function (event) {
-  $("#tab-contents .tab").hide();
-  $("#tab-menu .active").removeClass("active");
-  $($(this).attr("href")).show();
-  event.preventDefault();
+$(function () {
+  $("#btn").on("click", function () {
+    $.ajax({
+      url:
+        "http://api.openweathermap.org/data/2.5/weather?q=" +
+        $("#cityname").val() +
+        "&units=metric&appid=" +
+        API_KEY,
+      dataType: "jsonp",
+    })
+      .done(function (data) {
+        $("#place").text(data.name);
+        $("#temp_max").text(data.main.temp_max);
+        $("#temp_min").text(data.main.temp_min);
+        $("#humidity").text(data.main.humidity);
+        $("#speed").text(data.weather[0].main);
+        $("img").attr(
+          "src",
+          "http://openweathermap.org/img/w/" + data.weather[0].icon + ".png"
+        );
+        $("img").attr("alt", data.weather[0].main);
+      })
+      .fail(function (data) {});
+  });
 });
